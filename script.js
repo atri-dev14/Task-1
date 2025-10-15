1551769cd56084f71d8274327b79f4ba7a4e4d45
atri-dev14 <atrichandra2105@gmail.com>
2025-10-15 22:07:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    const myButton = document.getElementById('alertButton');
+
+    myButton.addEventListener('click', function() {
+        alert('Hello! You clicked the button!');
+    });
+
+});
